f0bece9cc27124674c6d6038ded6e9ba01cb913f
ahn-h-j <tkgkd246@gmail.com>
2026-01-07 14:46:16 +0900

    git init

---
+++ src/main/java/com/cos/fairbid/FairBidApplication.java
@@ -0,0 +1,13 @@
+package com.cos.fairbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairBidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FairBidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cos/fairbid/FairBidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.fairbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairBidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
