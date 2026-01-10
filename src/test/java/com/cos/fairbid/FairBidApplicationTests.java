04afb5e643c8d95040f2c043c046f245fa587079
ahn-h-j <tkgkd246@gmail.com>
2026-01-10 15:17:37 +0900

    fix(test): 테스트 컨텍스트 로드 실패 수정
    - TestRestTemplate 빈 주입 실패 문제 해결
    - FairBidApplicationTests에 webEnvironment = RANDOM_PORT 추가
    - @EnableJpaRepositories로 JPA Repository 스캔 경로 명시

--- src/test/java/com/cos/fairbid/FairBidApplicationTests.java
+++ src/test/java/com/cos/fairbid/FairBidApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @TestPropertySource(properties = {
         "spring.datasource.url=jdbc:tc:mysql:8.0:///testdb",
